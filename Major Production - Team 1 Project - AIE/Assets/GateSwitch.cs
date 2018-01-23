c3812bcc499f09424d2244698357e795ade6a442
Matty <mathewpalumbo86@gmail.com>
2018-01-23 20:11:11 +0800

    prototyping
    updated level intro
    added GateSwitch script to open generic gates
    added gate open sound effect

---
+++ Major Production - Team 1 Project - AIE/Assets/GateSwitch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateSwitch : MonoBehaviour {
+
+    public Transform Closed;
+    public Transform Open;
+    
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Civillian")
+        {
+            Closed.transform.position = Open.transform.position;
+            Closed.transform.rotation = Open.transform.rotation;
+        }
+    }
+
+}
