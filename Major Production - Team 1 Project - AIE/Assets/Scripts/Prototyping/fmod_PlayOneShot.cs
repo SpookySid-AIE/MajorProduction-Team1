25f5f724616fc758c76bc95b561461249178e898
Matty <mathewpalumbo86@gmail.com>
2018-01-30 21:42:18 +0800

    prototyping
    updated MattyTest3 with "lure through door" scenario
    added Sid laughing sfx (not in use)
    added fmod playoneshot script (not in use)

---
+++ Major Production - Team 1 Project - AIE/Assets/Scripts/Prototyping/fmod_PlayOneShot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fmod_PlayOneShot : MonoBehaviour {
+
+    // FMOD stuff - MP
+    [FMODUnity.EventRef]
+    public string scareSoundOnRepel = "event:/Sid_Scare_Laugh";
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        // This tells FMOD to play the sound event when the animation plays - MP                     
+        // FMODUnity.RuntimeManager.PlayOneShot(scareSoundOnRepel, this.gameObject.transform.position);
+    }
+}
