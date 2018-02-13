f8d795802c6fcbde7eea330843201f0f15685f5a
Jak Revai <jak.revai@students.aie.edu.au>
2018-02-13 13:32:21 +1000

    Revert "Merge branch 'master' of https://github.com/SpookySid-AIE/MajorProduction-Team1"
    This reverts commit 4692fbb89934bf7b1654bc8189bf1e500478532f, reversing
    changes made to 789f8f194f50d5b0cbef715fad4b4f0cd5b15e7e.

--- Major Production - Team 1 Project - AIE/Assets/Scripts/Player/playerController.cs
+++ Major Production - Team 1 Project - AIE/Assets/Scripts/Player/playerController.cs
@@ -16,7 +16,6 @@ public class playerController : MonoBehaviour
     [HideInInspector] public float ectoplasmValue; // What is this for? Why is this hidden? - MP
     public float speed = 6.0f;
     public float floatSpeed; //Speed of how fasty you float upwards when holding space.
-    public float sinkspeed = 1.0f;
 
     private Text txt_ectoplasm;
     private Vector3 moveDirection = Vector3.zero;
@@ -36,34 +35,23 @@ public class playerController : MonoBehaviour
         
         // Use input "W" and "S" for direction, multiplied by speed
         moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        var staticmove = moveDirection;
-       
-        moveDirection = transform.TransformDirection(moveDirection);
-        
-        moveDirection.y = 0;
+
         if (Input.GetKey(KeyCode.Space))
         {
-            moveDirection.y = floatSpeed;
+            moveDirection.y += floatSpeed;
         }
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            moveDirection.y = -sinkspeed;
+            moveDirection.y -= floatSpeed;
         }
-
+        moveDirection = transform.TransformDirection(moveDirection);
         moveDirection *= speed;
 
         // Move Character Controller
-        controller.Move(moveDirection * Time.deltaTime);
-
-        //tilt Sid over
-        var rot = transform.rotation;
-            rot.eulerAngles = transform.rotation.eulerAngles + new Vector3(staticmove.z * 5.0f, 0, staticmove.x * 5.0f);
-        //transform.rotation = rot;
+        if(controller.enabled == true)
+            controller.Move(moveDirection * Time.deltaTime);
     }
 
-
-
-    // Is this redundant? Shooting no longer uses game objects - MP
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Bullet")
