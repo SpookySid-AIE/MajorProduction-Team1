072ebf7b6c47f7fe7de4b4773f341cb7d725555c
burlpony <burlhazza@gmail.com>
2018-02-17 22:28:37 +1100

    More animation controllers
    added animation controllers for drink shelfs with new model and particle system for scare. Breadshelfs animation controller and updated model with animation, Fish tank animation controller with updated model and animation

---
+++ Major Production - Team 1 Project - AIE/Assets/Scripts/script_AnimationEventTriggerParticles.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_AnimationEventTriggerParticles : MonoBehaviour
+{
+
+    public ParticleSystem particle1;
+    public ParticleSystem particle2;
+
+    public bool playPaticle1 = false;
+    public bool playPaticle2 = false;
+
+
+
+    private void Update()
+    {
+        if (playPaticle1)
+        {
+            playPaticle1 = false;
+            particle1.Play();
+        }
+        if (playPaticle2)
+        {
+            playPaticle2 = false;
+            particle2.Play();
+        }
+
+
+    }
+}
