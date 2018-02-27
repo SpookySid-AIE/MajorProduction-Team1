4d5385e5cfb76d2a81a1155957295ca2b37f2cd5
Jak Revai <jak.revai@students.aie.edu.au>
2018-02-28 00:10:07 +1000

    ItemController Script + Sid Prefab updated to reduce range of possess

--- Major Production - Team 1 Project - AIE/Assets/Editor/ItemControllerEditor.cs
+++ Major Production - Team 1 Project - AIE/Assets/Editor/ItemControllerEditor.cs
@@ -1,40 +1,117 @@
 //////////////////////////////////////////////////////////////
 //// Author: <Jak Revai>                                     
-//// Date Created: <29/07/17>                               
+//// Date Created: <27/02/18>                               
 //// Brief: <Custom Editor for the ItemController>  
 //////////////////////////////////////////////////////////////
-//using System;
-//using UnityEditor;
-//using UnityEngine;
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
 
-//[CustomEditor(typeof(ItemController))]
-//public class ItemControllerEditor : Editor
-//{
+[CustomEditor(typeof(ItemController))]
+public class ItemControllerEditor : Editor
+{
+    string[] colliderList = new string[] { "BoxCollider", "SphereCollider", "CapsuleCollider", "MeshCollider" };
+    int colliderIndex = 0;
+    ItemController item;
 
-//    //string materialList;
-//    //string[] materialTypes = new string[] { "Glass", "Wood", "Steel" };
-//    //int materialIndex = 0;
+    public override void OnInspectorGUI()
+    {
+        //Draw the default inspector
+        item = (ItemController)target; //The current object being inspected
 
-//    public override void OnInspectorGUI()
-//    {
-//        //Draw the default inspector
-//        //DrawDefaultInspector();
-//        //ItemController item = (ItemController)target; //The current object being inspected
+        if (!isValid())
+        {
+            GUILayout.Label("--Choose Collider--");
 
-//        //Set the index to the previously selected index in the inspector
-//        //materialIndex = Array.IndexOf(materialTypes, item.materialChosen);
+            //If the choice is not in the array then the index will be -1 so set back to 0
+            if (colliderIndex < 0)
+                colliderIndex = 0;
 
-//        //If the choice is not in the array then the index will be -1 so set back to 0
-//        //if (materialIndex < 0)
-//        //    materialIndex = 0;
+            //Assign index based on selected item
+            colliderIndex = EditorGUILayout.Popup(colliderIndex, colliderList);
 
-//        //GUILayout.Label("\nChoose a Material Type from the drop down list.");
-//        //materialIndex = EditorGUILayout.Popup(materialIndex, materialTypes);
+            if (GUILayout.Button("Setup Item"))
+            {
+                item.tag = "Item";
+                AddCollider(item); //Add collider
+                item.gameObject.AddComponent<Rigidbody>();
 
-//        //item.materialChosen = materialTypes[materialIndex]; //Set the material chosen in the current item to be the one selected in the dropdown
+                //Set Correct shader to use - Will only be added if we are using proper materials and not just the default one, so we assume when we setup the item its on a correct model
+                if (item.GetComponentInChildren<Renderer>().sharedMaterial.name == "Default-Material")
+                {
+                    //Prevents material leakage
+                    Debug.LogError("Default-Material detected on item. Shader could not be set. Please 'Reset' Item, and change material so shader can be set.");
+                }
+                else //Else a proper material is found, then we set item - "proper" as in hopefully not a in-built unity one
+                {
+                    //Setup the aura possesionn shader textures - values will still needed to be tweaked
+                    item.GetComponentInChildren<Renderer>().sharedMaterial.shader = Shader.Find("Custom/Possession Aura2");
+                    Texture t = AssetDatabase.LoadAssetAtPath("Assets/Textures/Skull-blur2.png", typeof(Texture)) as Texture;
+                    item.GetComponentInChildren<Renderer>().sharedMaterial.SetTexture("_AuraTexture", t);
+                    item.GetComponentInChildren<Renderer>().sharedMaterial.SetTexture("_AuraTexture2", t);
+                }
+            }
+        }
+        else if (isValid())
+        {
+            DrawDefaultInspector(); //Once the valid components are one - draw the default script values
 
-//        //Save the changes back to the object
-//        //EditorUtility.SetDirty(target);
-//    }
+            GUIStyle style = new GUIStyle();
+            style.richText = true;
+            GUILayout.Label("<size=18><color=red><b>WARNING</b></color></size> - Removes components that make an <b>item</b>.", style);
 
-//}
+            if(GUILayout.Button("Reset Item")) //Reset the item
+            {
+                DestroyImmediate(item.GetComponent<Collider>());
+                DestroyImmediate(item.GetComponent<Rigidbody>());
+                item.tag = "Untagged";
+                EditorGUIUtility.ExitGUI(); //Cancel the next draw frame so we can delete the components without error
+            }
+        }
+    }
+
+    //If all checks pass then return true
+    bool isValid()
+    {
+        if (item.tag != "Item")
+            return false;
+
+        if (item.GetComponent<Collider>() == null)
+            return false;
+        else
+            return true;
+    }
+
+    //Method to add a collider based on index from drop down list
+    void AddCollider(ItemController item)
+    {
+        switch (colliderIndex)
+        {
+            case 0:
+                {
+                    //if(item.gameObject.GetComponent<BoxCollider>() == null)
+                        item.gameObject.AddComponent<BoxCollider>();
+                    break;
+                }
+            case 1:
+                {
+                    item.gameObject.AddComponent<SphereCollider>();
+                    break;
+                }
+            case 2:
+                {
+                    item.gameObject.AddComponent<CapsuleCollider>();
+                    break;
+                }
+            case 3:
+                {
+                    item.gameObject.AddComponent<MeshCollider>();
+                    break;
+                }
+            default:
+                break;
+        }
+    }
+
+}
