b610184158465044544bc47d1db46db8164054a3
Jak Revai <jak.revai@students.aie.edu.au>
2018-03-02 19:49:11 +1000

    Fix Shader glitch Highlight
    Highlight shader should now correctly update whether the civ is in the sphere or not.

--- Major Production - Team 1 Project - AIE/Assets/Shaders/Highlight/TriggerHighlight.cs
+++ Major Production - Team 1 Project - AIE/Assets/Shaders/Highlight/TriggerHighlight.cs
@@ -1,15 +1,22 @@
 ////////////////////////////////////////////////////////////
-// Author: <Jak Revai>                                     
-// Date Created: <00/00/00>                               
-// Brief: <Description>  
+// Author: <Jak Revai>                                                                 
+// Brief: <Simply trigger the changing of materials when a civ enters the collider>  
 ////////////////////////////////////////////////////////////
 using UnityEngine;
+using System.Collections.Generic;
 
 public class TriggerHighlight : MonoBehaviour
 {
     private Material highlightMaterial;
-    private Material old;
-    private Renderer render;
+
+    private struct CivValues
+    {
+        public Material oldMat;
+        public Renderer render; 
+    }
+
+    //Store all data for the material and renderer for EACH civ
+    Dictionary<int, CivValues> civData = new Dictionary<int, CivValues>();     
 
     private void Start()
     {
@@ -20,15 +27,35 @@ public class TriggerHighlight : MonoBehaviour
     {
         if (other.tag == "Civillian")
         {
-            render = other.GetComponentInChildren<Renderer>();
-            old = render.material;
-            render.material = highlightMaterial;
+            //Build struct data
+            CivValues v;
+            v.render = other.GetComponentInChildren<Renderer>();
+            v.oldMat = v.render.material;
+
+            //Add the data into the dictionary so we can easily look up this civ and change its material in OnExit
+            civData.Add(other.GetInstanceID(), v);
+
+            //Highlight the current civ
+            v.render.material = highlightMaterial;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Civillian")
-            render.material = old;
+        {
+            civData[other.GetInstanceID()].render.material = civData[other.GetInstanceID()].oldMat; //Revert highlight back to old mat
+            civData.Remove(other.GetInstanceID());
+        }
+    }
+
+    private void OnDestroy()
+    {
+        foreach (var item in civData) //Final catch to change material back to old mat incase luresphere is deleted early and not every civ has the material reverted
+        {
+            item.Value.render.material = item.Value.oldMat;
+        }
+
+        civData.Clear(); //needed?? because script is being destroyed surely this will empty as well
     }
 }
