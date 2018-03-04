620083bb7fa3bf2f1e5202d08c62debefda1dbce
Ben Thompson <onleeben@hotmail.com>
2018-03-04 21:43:06 +1000

    Menu update
    Menu works well now, besides pausing ingame

---
+++ Major Production - Team 1 Project - AIE/Assets/Scripts/changeMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeMenu : MonoBehaviour
+{
+
+    public GameObject controls;
+    public GameObject mainMenu;
+    public GameObject credits;
+    public GameObject pause;
+    public GameObject sid;
+
+    public void controlsSwap()
+    {
+        mainMenu.SetActive(false);
+        credits.SetActive(false);
+        pause.SetActive(false);
+
+        controls.SetActive(true);
+
+        sid.SetActive(false);
+    }
+
+    public void mainSwap()
+    {
+        controls.SetActive(false);
+        credits.SetActive(false);
+        pause.SetActive(false);
+
+        mainMenu.SetActive(true);
+
+        sid.SetActive(true);
+    }
+
+    public void creditSwap()
+    {
+        controls.SetActive(false);
+        mainMenu.SetActive(false);
+        pause.SetActive(false);
+        
+        credits.SetActive(true);
+
+        sid.SetActive(false);
+    }
+}
